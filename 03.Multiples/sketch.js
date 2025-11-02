c323ab6fa4b75daac857dbb4caf1f8826972cd2c
And339k <prodandee3900@gmail.com>
2025-11-02 17:18:24 +0000

    Add files via upload

---
+++ 03.Multiples/sketch.js
@@ -0,0 +1,25 @@
+let theText= ["cost", "1500", "for","the", "shoes"];
+let word = theText[0];
+let i=0;
+
+function setup() {
+createCanvas(400, 400);
+frameRate(5);
+}
+
+function draw() {
+  background(220);
+
+  // The text display
+  textSize(50);
+  text(word,random(400),height/2);
+
+  //change the value of word 
+  word=theText[i];
+  i=i+1;
+  if(i>=theText.length){
+    i=0;
+  }
+  console.log(i);
+
+}
